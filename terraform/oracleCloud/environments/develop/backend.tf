b60c4bc31da16c4ffbfb78a0a460f5ed78488bb5
James Forbes <james_forbes@live.co.uk>
2026-07-24 11:16:43 +0200

    backup terraform state to OCI cloud bucket

---
+++ terraform/oracleCloud/environments/develop/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+
+  backend "oci" {
+
+    bucket    = "terraform-state"
+
+    namespace = "lrlzwqok77yg"
+
+    region    = "uk-london-1"
+
+    key       = "cloudbills/develop/terraform.tfstate"
+
+  }
+}
